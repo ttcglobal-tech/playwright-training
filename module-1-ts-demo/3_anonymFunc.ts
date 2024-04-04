8b6ec639df2cc12546f22f92879b8fe67460dad8
Chandrakant Singh <chandrakant.singh@ttcglobal.com>
2024-04-05 09:46:15 +1300

    fix typescript demos

--- module-1-ts-demo/3_anonymFunc.ts
+++ module-1-ts-demo/3_anonymFunc.ts
@@ -2,3 +2,9 @@ const greetFunc = function (name: string): void {
   console.log("Hello, " + name.toUpperCase() + "!!");
 };
 greetFunc("Dave");
+
+//passing an anonymous function as a callback to the **forEach** method
+let numbers = [1, 2, 3, 4, 5];
+numbers.forEach(function(number) {
+  console.log(number);
+});
